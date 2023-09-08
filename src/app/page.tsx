b4660f4d1003354b998d5eb7b476657a24dfc1d0
Paul Phu <paulphu@gmail.com>
2023-09-07 23:05:36 -0700

    adding support for llama2 using Replicate by changing local env file. Fixed build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Navbar from "@/components/Navbar";
-import Examples from "@/components/Examples";
 import Tamagotchi from "@/components/Tamagotchi";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       {/* <Navbar /> */}
-      <Tamagotchi/>
+      <Tamagotchi />
     </main>
   );
 }
---
+++ src/app/utils/model.ts
@@ -0,0 +1,35 @@
+
+import { OpenAI } from "langchain/llms/openai";
+import { Replicate } from "langchain/llms/replicate";
+
+enum LlmModel {
+    OpenAI = 'openai',
+    ReplicateLlama = 'replicate_llama',
+}
+export function getModel() {
+    const model = (process.env.LLM_MODEL || "").toLowerCase()
+    console.log("model", model)
+    if (model == LlmModel.ReplicateLlama) {
+        console.debug("Using Replicate Llama")
+        return new Replicate({
+            apiKey: process.env.REPLICATE_API_TOKEN,
+            model:
+                //newer versions have emojis broken 😔
+                "meta/llama13b-v2-chat:2d19859030ff705a87c746f7e96eea03aefb71f166725aee39692f1476566d48",
+            input: {
+                system_prompt: `You are a helpful assistant, that only communicates using JSON files and no other words.
+                The expected output from you has to be: 
+                    {
+                        "text": {response}
+                    }
+                "`
+            }
+        })
+    } else { //default to OpenAI
+        console.debug("Using default (OpenAI)")
+        return new OpenAI({
+            modelName: "gpt-3.5-turbo-16k",
+            openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+    }
+}
