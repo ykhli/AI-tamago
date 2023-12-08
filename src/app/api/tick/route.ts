1a7a81e6b3a98533ae2ca5e5f91e9f808f5b6e23
Yoko Li <ykhli1013@gmail.com>
2023-12-08 09:15:13 -0800

    error

--- src/app/api/tick/route.ts
+++ src/app/api/tick/route.ts
@@ -1,10 +1,25 @@
 import dotenv from "dotenv";
+import arcjet, { rateLimit } from "@arcjet/next";
 import { NextResponse } from "next/server";
 import { inngest } from "../../../inngest/client";
 import { createClient } from "@supabase/supabase-js";
 import StateManager from "@/app/utils/state";
+import { NextApiResponse } from "next";
 
 dotenv.config({ path: `.env.local` });
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY!,
+  rules: [
+    rateLimit({
+      mode: "LIVE",
+      characteristics: ["ip.src"],
+      window: "1m",
+      max: 1,
+      timeout: "10m",
+    }),
+  ],
+});
 
 export async function POST(req: Request) {
   // TODO - this should be invoked every second or minute to update tamagotchi status. Things like:
@@ -13,6 +28,18 @@ export async function POST(req: Request) {
   // Get sick
   // Sleep (based on clock)
   // .... or other things LLM can think of...
+  const decision = await aj.protect(req);
+  if (decision.isDenied()) {
+    return NextResponse.json(
+      {
+        error: "Too Many Requests",
+        reason: decision.reason,
+      },
+      {
+        status: 429,
+      }
+    );
+  }
 
   const url = process.env.SUPABASE_URL!;
   const privateKey = process.env.SUPABASE_PRIVATE_KEY!;
